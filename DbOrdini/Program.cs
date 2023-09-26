4e8be5f6bb490dad602d29fe90a51b87f2d06643
Davide Mattavelli <Davide.mattavelli@beta80group.it>
2023-09-26 12:06:21 +0200

    Add project files.

---
+++ DbOrdini/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbOrdini
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public static void Login(string a, string b, SqlConnection c)
+        {
+            using (c)
+            {
+                //login 
+                Console.WriteLine($"connessione = {c}");
+                c.Open();
+                Console.WriteLine("connessione aperta");
+                Console.WriteLine("effettua il login per accedere al db");
+                
+                var cmd = new SqlCommand($"select * from users where username=@username and password=@password", c);
+                cmd.Parameters.Add(new SqlParameter("username", a));
+                cmd.Parameters.Add(new SqlParameter("password", b));
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+        public static void CreaUtente(string a, string b, SqlConnection c)
+        {
+            using (c)
+            {
+                Console.WriteLine($"connessione = {c}");
+                c.Open();
+                Console.WriteLine("connessione aperta");
+                var cmd = new SqlCommand($"select * from users where username=@username and password=@password", c);
+                //creazione utente 
+                Console.WriteLine("crea nuovo utente");
+                cmd = new SqlCommand($"insert into utenti values ({a}, {b})", c);
+
+                cmd.Parameters.Add(a);
+                cmd.Parameters.Add(b);
+            }
+            Console.ReadLine();
+
+        }
+
+        public static void ListaOrdini(string a, string b, SqlConnection c)
+        {
+            Console.WriteLine($"connessione = {c}");
+            c.Open();
+            Console.WriteLine("connessione aperta");
+
+            var cmd = new SqlCommand("select * from orders");
+
+            using (var orders = cmd.ExecuteReader())
+
+            {
+                while (orders.Read())
+                {
+                    Console.WriteLine("{0} {1}", orders["orderid"], orders["costumer"]);
+                }
+            }
+            Console.ReadLine();
+
+        }
+
+        public static void DettagliOrdine(string a, string b, SqlConnection c)
+        {
+            using (c)
+            {
+                string a = ("select count(*) from orderitems");
+                var cmd = new SqlCommand(a, c);
+                Console.WriteLine($"ci sono {a} righe di ordini ");
+                cmd = new SqlCommand("select * from orderitems", c);
+                using (var orderitems = cmd.ExecuteReader())
+
+                {
+                    while (orderitems.Read())
+                    {
+                        Console.WriteLine("{0} {1}", orderitems["orderid"], orderitems["item"], orderitems["qty"], orderitems["price"]);
+                    }
+                }
+                Console.ReadLine();
+
+
+
+            }
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "data source=.\\SQLEXPRESS; initial catalog=orders; User ID=sa; Password=sa;";
+            SqlConnection con = new SqlConnection(connStr);
+            Console.WriteLine($"connessione = {con}");
+            con.Open();
+            Console.WriteLine("connessione aperta");
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            User.Login(username, password, con);
+            User.CreaUtente(username, password, con);
+            User.ListaOrdini(username, password, con);
+            User.DettagliOrdine(username, password, con);
+       
+            
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
---
+++ esercizio1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace esercizio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            string connStr = "data source=.\\SQLEXPRESS; initial catalog=orders; User ID=sa; Password=sa;";
+            SqlConnection con = new SqlConnection(connStr);
+            using (con)
+            {
+
+                Console.WriteLine($"connessione = {con}");
+                con.Open();
+                Console.WriteLine("connessione aperta");
+                string q = "select count(*) from orders";
+                var cmd = new SqlCommand(q, con);
+                var n = cmd.ExecuteScalar();
+                Console.WriteLine($"ci sono {n} ordini");
+
+                cmd = new SqlCommand("select * from orders", con);
+                using (var orders = cmd.ExecuteReader())
+
+                {
+                    while (orders.Read())
+                    {
+                        Console.WriteLine("{0} {1}", orders["orderid"], orders["customer"]);
+                    }
+                }
+                //ricerca utenti tramite injection
+                Console.WriteLine("digita nome utente");
+                string user = Console.ReadLine();
+                cmd = new SqlCommand($"select * from orders where customer = '{user}'", con);
+                SqlParameter par = new SqlParameter("@user", SqlDbType.NVarChar, 50);
+                Console.WriteLine("con parametro");
+                cmd.Parameters.Add(par);
+                par.Value = user;
+                using (var orders = cmd.ExecuteReader())
+                {
+                    while (orders.Read())
+                    {
+                        Console.WriteLine("->{0} {1}", orders["orderid"], orders["customer"]);
+                    }
+                }
+                //modifica dati del prezzo di un certo tipo di utenti
+                SqlTransaction tr = null;
+                try
+                {
+                tr = con.BeginTransaction(); //transazione 
+                Console.WriteLine("UPDATE");
+                cmd = new SqlCommand($"update orderitems set price=price+100 where orderid=@order", con, tr);
+                cmd.Parameters.Add(new SqlParameter("@order", 1));
+                Console.WriteLine($"ho modificato {cmd.ExecuteNonQuery()} righe");
+                tr.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    tr.Rollback();
+                }
+
+
+                Console.WriteLine("UPDATE");
+                cmd = new SqlCommand($"update orderitems set price=price+100 where orderid=@order", con);
+                cmd.Parameters.Add(new SqlParameter("@order", 1));
+                Console.WriteLine($"ho modificato {cmd.ExecuteNonQuery()} righe");
+                //ADAPTER
+                SqlDataAdapter adapter = new SqlDataAdapter("select * from customers", con);
+                DataSet model = new DataSet();
+                adapter.Fill(model, "customers");
+                Console.WriteLine("carico il dataset del customer");
+                foreach (DataRow c in model.Tables["customers"].Rows)
+                {
+                    Console.WriteLine("{0} {1}", c["customer"], c["country"]);
+                }
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
